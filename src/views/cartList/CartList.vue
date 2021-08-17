ae9ddd3af5659e9fddf39a8948ae6f3088f7799d
zl轻烟 <5342143+zlqingyan@user.noreply.gitee.com>
2021-08-17 15:11:19 +0800

    takeout

---
+++ src/views/cartList/CartList.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="cart">cart</div>
+</template>
+
+<script>
+export default {
+  name: 'CartList'
+}
+</script>
+
+<style lang="scss" scoped>
+.cart {
+  margin-top: .2rem;
+  text-align: center;
+}
+</style>
